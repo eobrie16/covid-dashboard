5d44231d5a6b7c61ae66c2b39ba15bcc329971d2
Ed OBrien <late.for.lunch@gmail.com>
2020-05-23 02:28:53 -0400

    Added bokeh color map

--- covid_app.py
+++ covid_app.py
@@ -1,11 +1,14 @@
 import math
 import numpy as np
 import pandas as pd
+import json
 
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource, HoverTool, PrintfTickFormatter
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, LogColorMapper, ColorBar
 from bokeh.plotting import figure
 from bokeh.transform import factor_cmap
+from bokeh.palettes import brewer
 
 from flask import Flask, render_template, request
 
@@ -57,15 +60,13 @@ def chart():
     selected_class = request.form.get('dropdown-select')
 
     if selected_class == 0 or selected_class == None:
-        # cases_chart, death_chart, tests_chart = redraw(1)
-        cases_chart, death_chart = redraw('total')
+        cases_chart, death_chart, us_map = redraw('total')
     else:
-        # cases_chart, death_chart, tests_chart = redraw(selected_class)
-        cases_chart, death_chart = redraw(selected_class)
+        cases_chart, death_chart, us_map = redraw(selected_class)
 
     script_cases_chart, div_cases_chart = components(cases_chart)
     script_death_chart, div_death_chart = components(death_chart)
-    # script_tests_chart, div_tests_chart = components(tests_chart)
+    script_us_map, div_us_map = components(us_map)
 
     return render_template(
         'index.html',
@@ -73,12 +74,12 @@ def chart():
         script_cases_chart=script_cases_chart,
         div_death_chart=div_death_chart,
         script_death_chart=script_death_chart,
-        # div_tests_chart=div_tests_chart,
-        # script_tests_chart=script_tests_chart,
+        div_us_map=div_us_map,
+        script_us_map=script_us_map,
         selected_class=selected_class
     )
 
-def bar_chart(dataset, type, depend, cpalette=palette, count=15):
+def process_data(dataset, type, depend, count=None):
     factor = 1
     if type == 'capita':
         type_str = f'pc_{depend}'
@@ -95,10 +96,19 @@ def bar_chart(dataset, type, depend, cpalette=palette, count=15):
         type_str = depend
         title = depend.title()
         
-    cutoff = dataset.sort_values(by=[type_str], ascending=False).iloc[count][type_str]
-    the_data = dataset[dataset[type_str] > cutoff]
+    if count:
+        cutoff = dataset.sort_values(by=[type_str], ascending=False).iloc[count][type_str]
+        the_data = dataset[dataset[type_str] > cutoff]
+    else:
+        the_data = dataset
+    the_data['values'] = factor*the_data[type_str]
+
+    return the_data, title
+
+def bar_chart(dataset, type, depend, cpalette=palette, count=15):
+    the_data, title = process_data(dataset, type, depend, count)
         
-    values = list(factor*the_data[type_str].values)
+    values = list(the_data['values'])
     states = list(the_data['state'])
         
     source = ColumnDataSource(data={
@@ -123,38 +133,48 @@ def bar_chart(dataset, type, depend, cpalette=palette, count=15):
     
     return p
 
-# def tests_hist(dataset, pass_class, color=palette[1]):
-#     hist, edges = np.histogram(dataset[dataset['Pclass'] == int(pass_class)]['Age'].fillna(df['Age'].mean()), bins=25)
+def create_map(dataset, type, depend, cpalette=palette):
+    the_data, title = process_data(dataset, type, depend)
+
+    #Read data to json.
+    merged_json = json.loads(the_data.to_json())
+    json_data = json.dumps(merged_json)
+    geosource = GeoJSONDataSource(geojson = json_data)
+    #Define a sequential multi-hue color palette.
+    palette = brewer['YlGnBu'][8]
+    palette = palette[::-1]
+    #Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
+    max_cases = max(the_data['values'])
+    min_cases = min(the_data['values'])
+    color_mapper = LinearColorMapper(palette = palette, low = min_cases, high = max_cases)
+    #Create color bar. 
+    # color_bar = ColorBar(color_mapper=color_mapper, width = 500, height = 20,
+    # border_line_color=None,location = (0,0), orientation = 'horizontal')
+
+    tools = "pan,wheel_zoom,reset,hover"
+
+    p = figure(title = title, tools=tools, plot_height = 600 , plot_width = 950,
+               toolbar_location = None, x_axis_location=None, y_axis_location=None,
+               tooltips=[("State", "@state"), ("Count", "@values")])
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None  
+    p.hover.point_policy = "follow_mouse"
+    p.patches('xs','ys', source = geosource, fill_color = {'field' : 'values', 'transform' : color_mapper},
+          line_color = 'black', line_width = 0.25, fill_alpha = 1)
+    #Specify figure layout.
+    # p.add_layout(color_bar, 'below')
     
-#     source = ColumnDataSource({
-#         'hist': hist,
-#         'edges_left': edges[:-1],
-#         'edges_right': edges[1:]
-#     })
-
-#     hover_tool = HoverTool(
-#         tooltips=[('From', '@edges_left'), ('Thru', '@edges_right'), ('Count', '@hist')], 
-#         mode='vline'
-#     )
-    
-#     p = figure(plot_height=400, title='Age Histogram', tools=[hover_tool])
-#     p.quad(top='hist', bottom=0, left='edges_left', right='edges_right', source=source,
-#             fill_color=color, line_color='black')
-
-#     plot_styler(p)
-#     p.sizing_mode = 'scale_width'
-
-#     return p
+    return p
 
 def redraw(p_class):
     dataset = get_data()
     cases_chart = bar_chart(dataset, p_class, 'cases')
     death_chart = bar_chart(dataset, p_class, 'death')
-    # tests_chart = age_hist(df, p_class)
+    us_map = create_map(dataset, p_class, 'cases')
     return (
         cases_chart,
         death_chart,
-        # tests_chart
+        us_map
     )
 
 
--- covid_data.py
+++ covid_data.py
@@ -1,9 +1,11 @@
 import math
 import pandas as pd
+import geopandas as gpd
 import us
 
 COVID_CURRENT = r"https://covidtracking.com/api/v1/states/current.csv"
 COVID_HISTORY = r"https://covidtracking.com/api/v1/states/daily.csv"
+US_MAP = "data/ne_50m_admin_1_states_provinces/ne_50m_admin_1_states_provinces.shp"
 
 
 def get_covid():
@@ -35,10 +37,24 @@ def get_pop():
 
     return pop
 
+def get_map():
+    gdf = gpd.read_file(US_MAP)[['iso_3166_2', 'name', 'geometry']]
+    gdf.columns = ['abbr', 'name', 'geometry']
+    gdf = gdf[gdf['abbr'].str.contains('US')]
+    gdf['abbr'] = gdf['abbr'].apply(lambda x: x[3:])
+    # remove AK/HI to scale map
+    gdf = gdf[(gdf['abbr'] != 'AK') & (gdf['abbr'] != 'HI')]
+
+    return gdf
+
+
 def get_data():
     # merge covid and population data
     cur_pop = get_covid().merge(right=get_pop(), left_on=['state'], right_on=['NAME'])
     cur_pop.drop(['NAME'], axis=1)
+
+    # merge map data
+    cur_pop = get_map().merge(cur_pop, left_on='abbr', right_on='state')
 
     #per capita columns
     cur_pop['pc_cases'] = cur_pop['cases'] / cur_pop['population']
